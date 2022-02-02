04cd27cc899d15a53c393b6f649fad7d1576b9fb
GANESH2903 <97089641+GANESH2903@users.noreply.github.com>
2022-02-02 06:11:29 +0000

    p3 original

---
+++ p3original.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int input_n()
+{
+  int n;
+  printf("To find sum of n numbers\n");
+  printf("Enter value of n\n");
+  scanf("%d",n);
+  return n;
+}
+int sum_n(int n)
+{
+  int sum=0,i;
+  for(i=0;i<=n;i++);
+  {
+    sum = sum + i;
+  }
+  return sum;
+}
+void output(int n,int sum)
+{
+  printf(" the of first %d numbers is\t",n);
+  int i;
+  for (i=1;i<n;i++);
+  printf("%d+\t",i);
+  printf("%d\t",n);
+  printf("= %d \n",sum);
+
+}
+
+int main()
+{
+  int i,n,sum;
+  n = input_n();
+  sum = sum_n(n);
+  output(n,sum);
+  return 0;
+}
